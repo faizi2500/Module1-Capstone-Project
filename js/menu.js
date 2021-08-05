7284d0f860d5222575a0bbe5b7f25d9aff78eac1
Faizan <faizan.zahid2500@gmail.com>
2021-08-05 06:23:36 +0500

    Linters fixed

--- js/menu.js
+++ js/menu.js
@@ -1,29 +1,18 @@
 const openMenu = document.getElementById('menu');
 const closeMenu = document.getElementById('close');
 const menu = document.getElementById('mob-nav');
-const logo = document.getElementsByClassName('logo-desktop')
-const navWidth =document.getElementsByClassName('navigation-container');
-
-const status = false;
-
-
 
 function openMobileMenu() {
   menu.style.display = 'block';
-  menu.style.width = '100%';
-  openMenu.style.display = 'none'
-  closeMenu.style.display = 'block'
-  logo.style.display = 'block'
-  status = true;
-  
+  openMenu.style.display = 'none';
+  closeMenu.style.display = 'block';
 }
 
 function closeMobileMenu() {
   menu.style.display = 'none';
-  openMenu.style.display = 'block'
-  closeMenu.style.display = 'none'
+  openMenu.style.display = 'block';
+  closeMenu.style.display = 'none';
 }
 
 openMenu.addEventListener('click', openMobileMenu);
 closeMenu.addEventListener('click', closeMobileMenu);
-
--- js/section.js
+++ js/section.js
@@ -1,4 +1,3 @@
-
 const data = [
   {
     id: 1,
@@ -6,7 +5,7 @@ const data = [
     altText: 'This is featured speaker image',
     nameOfSpeaker: 'Chamath',
     designation: 'Canadian venture capitalist',
-    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.'
+    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.',
   },
   {
     id: 2,
@@ -14,7 +13,7 @@ const data = [
     altText: 'This is featured speaker image',
     nameOfSpeaker: 'Travis',
     designation: 'Uber Founder',
-    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.'
+    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.',
   },
   {
     id: 3,
@@ -22,7 +21,7 @@ const data = [
     altText: 'This is featured speaker image',
     nameOfSpeaker: 'Peter Thiel',
     designation: 'Former founder of PayPal',
-    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.'
+    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.',
   },
   {
     id: 4,
@@ -30,7 +29,7 @@ const data = [
     altText: 'This is featured speaker image',
     nameOfSpeaker: 'Chamath',
     designation: 'Canadian venture capitalist',
-    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.'
+    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.',
   },
   {
     id: 5,
@@ -38,7 +37,7 @@ const data = [
     altText: 'This is featured speaker image',
     nameOfSpeaker: 'David Sacks',
     designation: 'Canadian venture capitalist',
-    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.'
+    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.',
   },
   {
     id: 6,
@@ -46,50 +45,21 @@ const data = [
     altText: 'This is featured speaker image',
     nameOfSpeaker: 'Toni',
     designation: 'Shopify Founder',
-    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.'
-  }
+    history: 'He is a venture capitalist, engineer, SPAC sponsor and the founder and CEO of Social Capital. Palihapitiya was an early senior executive at Facebook, working at the company from 2007 to 2011.',
+  },
 ];
-
-let counter = data.length;
-
-// for (let i=0; i < data.length; i++){
-//   counter =counter + 1;
-//   if(counter >= 2 && max length of 768){
-
-
-//   }
-//   console.log(counter);
-// }
-
-
 
 const speakerContainer = document.createElement('section');
 speakerContainer.setAttribute('class', 'featured-speakers');
 speakerContainer.setAttribute('id', 'speakers');
 
-// const sectHeading = document.createElement('h2');
-// sectHeading.setAttribute('class', 'feature-heading');
-// sectHeading.textContent ='Featured Speakers';
-
-// speakerContainer.appendChild(sectHeading);
-
-
-data.forEach((speakerPara)=> {
-
-// for(let i=0; i < )
-  
-  
+data.forEach((speakerPara) => {
   const speakerSection = document.createElement('div');
   speakerSection.setAttribute('class', 'speaker-section');
   speakerSection.setAttribute('id', 'each-speaker');
 
   const imageSection = document.createElement('div');
   imageSection.setAttribute('class', 'speaker-image-section');
-  
-  // const imagebg = document.createElement('i');
-  // imagebg.setAttribute('class', 'fas fa-chess-board');
-  // speakerSection.appendChild(imagebg);
-
   const image = document.createElement('img');
   image.setAttribute('class', 'speaker-image');
   image.setAttribute('src', speakerPara.pic);
@@ -97,49 +67,35 @@ data.forEach((speakerPara)=> {
 
   imageSection.appendChild(image);
   speakerSection.appendChild(imageSection);
-  
+
   const contentSection = document.createElement('div');
   contentSection.setAttribute('class', 'speaker-details');
   contentSection.setAttribute('id', 'about-speaker');
 
   const speakerName = document.createElement('h2');
-  speakerName.setAttribute('class','speaker-name');
+  speakerName.setAttribute('class', 'speaker-name');
   speakerName.textContent = speakerPara.nameOfSpeaker;
   contentSection.appendChild(speakerName);
-  
-  const speakerDesignation =document.createElement('p')
-  speakerDesignation.setAttribute('class','speaker-designation');
+
+  const speakerDesignation = document.createElement('p');
+  speakerDesignation.setAttribute('class', 'speaker-designation');
   speakerDesignation.textContent = speakerPara.designation;
   contentSection.appendChild(speakerDesignation);
 
   const dotText = document.createElement('p');
   dotText.setAttribute('id', 'dots');
-  dotText.textContent = ".........";
+  dotText.textContent = '.........';
   contentSection.appendChild(dotText);
 
-  const speakerHistory =document.createElement('p')
-  speakerHistory.setAttribute('class','speaker-history');
+  const speakerHistory = document.createElement('p');
+  speakerHistory.setAttribute('class', 'speaker-history');
   speakerHistory.textContent = speakerPara.history;
   contentSection.appendChild(speakerHistory);
 
   speakerSection.appendChild(contentSection);
   speakerContainer.appendChild(speakerSection);
-  
-  // const body = document.getElementsByTagName('body');
+});
 
-  // document.body.appendChild(speakerContainer);
-})
-
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 const partners = document.getElementById('partners');
-body.insertBefore(speakerContainer, partners) 
-
-
-
-
-
-
-
-
-
-
+body.insertBefore(speakerContainer, partners);
